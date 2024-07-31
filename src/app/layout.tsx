08770e6fae1f1b5ee5cad86115131d30b6e71c17
mkaidev <mkaidev88@gmail.com>
2024-08-01 03:04:03 +0700

    upload file dialog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "./ConvexClientProvider";
 import { Header } from "./header";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ConvexClientProvider>
+          <Toaster />
           <Header />
           {children}
         </ConvexClientProvider>
